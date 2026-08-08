f0b7a2200dc925c0f290fb2527cb01d64191f2eb
BhavneetS <41502882+BhavneetS@users.noreply.github.com>
2026-08-08 20:18:36 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        // caret inside the bracket means negation, outside means beginning of the string, g flag means match all occurences
+        const filteredString = s.replace(/[^a-zA-z0-9]/g, "");
+        const targetString = filteredString.toLowerCase()
+        let j = targetString.length - 1;
+        for(const [i, char] of Array.from(targetString).entries()) {
+            if(i < j && char !== targetString[j]) {
+                return false
+                }
+            j--;
+            console.log(`j after reduction ${j}`)
+        }
+        return true;
+    }
+}
